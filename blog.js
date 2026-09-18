c185ab33d635b20156741320bd9d12d26cbbdbfb
alkhasusan <alkhasusansaju@gmail.com>
2026-09-18 19:14:19 +0530

    Fix BUG-HD3

--- blog.js
+++ blog.js
@@ -30,7 +30,7 @@ readBtns.forEach(function (btn) {
 const form = document.querySelector("[data-newsletter]");
 const emailInput = form.querySelector("input");
 const msg = form.querySelector("[data-msg]");
-const emailPattern = /^[a-z]+@[a-z]+\.[a-z]+$/;
+const emailPattern = /^[a-z0-9_]+@[a-z]+\.[a-z]+$/;
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   if (emailPattern.test(emailInput.value)) {
